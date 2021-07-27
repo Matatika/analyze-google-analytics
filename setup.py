219d148bfe1049dcc160091078ab5bdac1e6af15
danielpdwalker <danielpdwalker@gmail.com>
2021-07-27 17:02:19 +0100

    Renamed setup name in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="matatika-datasets-google-analytics",
+    name="analyze-google-analytics",
     version="0.1.0",
     description="Meltano project file bundle of Matatika datasets for Google Analytics",
     packages=find_packages(),
