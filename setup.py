b370db3c184350122270258370229d332c8f7d24
danielpdwalker <danielpdwalker@gmail.com>
2022-12-05 15:14:27 +0000

    Updated all .yaml files to .yml

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="analyze-google-analytics",
-    version="0.4.0",
+    version="0.4.1",
     description="Meltano project file bundle of Matatika datasets for Google Analytics",
     packages=find_packages(),
     package_data={
         "bundle": [
-            "analyze/datasets/tap-google-analytics/*.yaml"
+            "analyze/datasets/tap-google-analytics/*.yml"
         ]
     },
 )
